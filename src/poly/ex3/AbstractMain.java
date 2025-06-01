ae9770fb59093a7da80dcef071ef1095c11156ea
nsjjjjjj <nsj050524@gmail.com>
2025-06-01 22:54:15 +0900

    6/1

---
+++ src/poly/ex3/AbstractMain.java
@@ -0,0 +1,27 @@
+package poly.ex3;
+
+import poly.ex2.Animal;
+
+public class AbstractMain {
+    public static void main(String[] args) {
+        //AbstractAnimal animal = new Animal();
+
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+        Caw caw = new Caw();
+        Duck duck = new Duck();
+
+        soundAnimal(dog);
+        soundAnimal(cat);
+        soundAnimal(caw);
+        soundAnimal(duck);
+
+        caw.move();
+    }
+
+    private static void soundAnimal(AbstractAnimal animal) {
+        System.out.println("동물 소리 테스트 시작");
+        animal.sound();
+        System.out.println("동물 소리 테스트 종료");
+    }
+}
---
+++ src/poly/ex5/InterfaeceAnimal.java
@@ -0,0 +1,6 @@
+package poly.ex5;
+
+public interface InterfaeceAnimal {
+    void sound();
+    void move();
+}
